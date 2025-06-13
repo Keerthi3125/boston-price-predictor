7b5d3f5b6a631e58d0b9ff7d4326c0c47da59a77
KEERTHI SAI KUCHI <234g1a0569@srit.ac.in>
2025-06-13 21:16:45 +0530

    app.py

--- app.py
+++ app.py
@@ -21,5 +21,8 @@ def index():
             prediction = "Invalid input."
     return render_template("index.html", prediction=prediction)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
